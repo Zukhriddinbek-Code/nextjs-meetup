d558dddde37060320d6428d46ca9cbfb036c3b0d
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-08-28 18:58:08 +0900

    installing simple api routes to our backend to post coming request data to mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -2,15 +2,27 @@
 import { MongoClient } from "mongodb";
 // The MongoClient class is a class that allows for making Connections to MongoDB.
 
-const Handler = (req, res) => {
+const Handler = async (req, res) => {
   if (req.method === "POST") {
-    const data = req.body; //this holds the body (data) of incoming request
+    //this holds the body (data) of incoming request
+    const data = req.body;
 
-    const { title, image, address, description } = data;
-
-    MongoClient.connect(
+    //connecting coming request (Data) to mongodb
+    const client = await MongoClient.connect(
       "mongodb+srv://zuhriddin_ganiev:zuhriddin-tech@cluster-zuhriddin.65mbqpl.mongodb.net/?retryWrites=true&w=majority"
     );
+    const db = client.db();
+
+    //creating collection with name of 'meetups'
+    const meetupsCollection = db.collection("meetups");
+
+    const result = meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    //sending back response status and object
+    res.status(201).json({ message: "Meetup inserted" });
   }
 };
 
